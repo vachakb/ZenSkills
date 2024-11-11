7ecd58f608d998bde5aa021ca92e459957603983
Vacha <vachakb@gmail.com>
2024-11-12 02:40:15 +0530

    Router and upcoming page setup

---
+++ src/Upcoming.jsx
@@ -0,0 +1,6 @@
+function Upcoming(){
+    return <h1>
+        Eugiebear and Kittycha
+    </h1>
+}
+export default Upcoming;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Upcoming from './Upcoming.jsx'
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App/>,
+    children: [
+      {
+        path:'upcoming',
+        element: <Upcoming/>
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
