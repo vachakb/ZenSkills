14a27d91e7fff7fdc1816e96b9202f87ebbfd1e5
jainish <jdp5420950@gmail.com>
2024-11-17 02:36:57 +0530

    customised selected filters

--- frontend/src/components/MentorCard.jsx
+++ frontend/src/components/MentorCard.jsx
@@ -17,47 +17,49 @@ import demoMentorImage from "../assets/mentorImage.png";
 export default function mentorCard({ mentor }) {
   return (
     <div className="card col-12 col-sm-6 col-md-4 col-lg-3">
-        <div className="card-body">
+      <div className="card-body">
         <img
-        src={demoMentorImage}
-        alt="Mentor Image"
-        style={{ width: "100%", borderRadius: "10px" }}
-      />
+          src={demoMentorImage}
+          alt="Mentor Image"
+          style={{ width: "100%", borderRadius: "10px" }}
+        />
 
-      <div className="d-flex justify-content-between align-items-center">
-        <div className="fs-4 fw-bold card-title">{mentor.name}</div>
-        <div className="d-flex align-items-center" style={{ color: "yellow" }}>
-          <FaStar size={15} color="yellow" />
-          <span className="ms-2">{mentor.rating}</span>
-        </div>
-      </div>
-
-      <div className="d-flex align-content-center">
-        <FaBriefcase size={30} />
-        <span className="ps-2">{mentor.currentPost}</span>
-      </div>
-
-      <div>
-        <FaRegComment size={24} />
-        <span className="ps-2">
-          {mentor.noOfSessions} Sessions ({mentor.noOfReviews} Reviews)
-        </span>
-      </div>
-
-      <hr />
-
-      <div className="d-flex justify-content-around">
-        <div className="flex-fill text-center flex-column">
-          <div>Experience</div>
-          <div>{mentor.Experience} Years</div>
-        </div>
-        <div className="flex-fill text-center flex-column">
-          <div>Credit Score</div>
-          <div>{mentor.creditScore}</div>
-        </div>
-      </div>
-
-        </div>
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="fs-4 fw-bold card-title">{mentor.name}</div>
+          <div
+            className="d-flex align-items-center"
+            style={{ color: "yellow" }}
+          >
+            <FaStar size={15} color="yellow" />
+            <span className="ms-2">{mentor.rating}</span>
           </div>
+        </div>
+
+        <div className="d-flex align-content-center">
+          <FaBriefcase size={30} />
+          <span className="ps-2">{mentor.currentPost}</span>
+        </div>
+
+        <div>
+          <FaRegComment size={24} />
+          <span className="ps-2">
+            {mentor.noOfSessions} Sessions ({mentor.noOfReviews} Reviews)
+          </span>
+        </div>
+
+        <hr />
+
+        <div className="d-flex justify-content-around">
+          <div className="flex-fill text-center flex-column">
+            <div>Experience</div>
+            <div>{mentor.Experience} Years</div>
+          </div>
+          <div className="flex-fill text-center flex-column">
+            <div>Credit Score</div>
+            <div>{mentor.creditScore}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- frontend/src/pages/ExloreMentors.jsx
+++ frontend/src/pages/ExloreMentors.jsx
@@ -13,7 +13,8 @@ export default function ExploreMentor({ mentors_, demoTags }) {
   const [noOfMenteesMentored, setNoOfMenteedMentored] = useState(0);
   const [currentPage, setCurrntpage] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
-  const [filterDropdownVisibility, setFilterDropdownVisibility] = useState(false);
+  const [filterDropdownVisibility, setFilterDropdownVisibility] =
+    useState(false);
   const itemsPerPage = 10;
 
   // useEffect(()=>{
@@ -70,18 +71,18 @@ export default function ExploreMentor({ mentors_, demoTags }) {
     fetchTags();
   }, []);
 
-  function toggleFilterDropdownVisibility(){
-    if(filterDropdownVisibility)    setFilterDropdownVisibility(false)
-    else                            setFilterDropdownVisibility(true)
+  function toggleFilterDropdownVisibility() {
+    if (filterDropdownVisibility) setFilterDropdownVisibility(false);
+    else setFilterDropdownVisibility(true);
   }
 
-  function handleTagClick(tag){
-    if(selectedTags.includes(tag)){
-        setSelectedTags(selectedTags.filter((Tag)=> Tag!==tag))
-    }else{
-        setSelectedTags([...selectedTags, tag])
+  function handleTagClick(tag) {
+    if (selectedTags.includes(tag)) {
+      setSelectedTags(selectedTags.filter((Tag) => Tag !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
     }
-    console.log(selectedTags)
+    console.log(selectedTags);
   }
 
   return (
@@ -106,28 +107,84 @@ export default function ExploreMentor({ mentors_, demoTags }) {
           </button>
         </div>
         <div className="col-12 col-md-2">
-          <button className="btn btn-primary w-100" onClick={toggleFilterDropdownVisibility}>Filter</button>
+          <button
+            className="btn btn-primary w-100"
+            onClick={toggleFilterDropdownVisibility}
+          >
+            Filter
+          </button>
         </div>
       </div>
 
       {filterDropdownVisibility && (
         <div className="mb-4 border p-2">
-          Select Tags <br />
+          <div className="d-flex justify-content-between">
+            <div>Select Tags</div>
+            <button
+              className="btn-close"
+              aria-label="Close"
+              onClick={toggleFilterDropdownVisibility}
+            ></button>
+          </div>
           {allTags.map((tag) => {
-            return <button className="btn btn-sm bg-body-secondary rounded-pill m-1" onClick={()=>handleTagClick(tag)}>{tag}</button>;
+            return (
+              <button
+                className={`btn btn-sm rounded-pill m-1`}
+                style={{
+                  backgroundColor: selectedTags.includes(tag)
+                    ? "#07d100"
+                    : "rgb(233, 236, 239)",
+                }}
+                onClick={() => handleTagClick(tag)}
+              >
+                {tag}
+              </button>
+            );
           })}
         </div>
       )}
 
-      {
-        selectedTags?.length!==0 &&
-        <div>
-            Filters applied:
-            {selectedTags.map((tag)=>{
+      {selectedTags?.length !== 0 && (
+        <div className="">
+          Filters applied:
+          {/* {selectedTags.map((tag)=>{
                 return <button className="btn btn-sm bg-body-secondary rounded-pill m-1" onClick={()=>handleTagClick(tag)}>{tag}</button>;
-            })}
+            })} */}
+          {selectedTags.map((tag) => {
+            return (
+              <div
+                className="rounded-pill ps-2 m-1 d-inline-block"
+                style={{ backgroundColor: "rgb(233, 236, 239)" }}
+              >
+                <span>{tag}</span>
+                <button type="button" className="btn"
+                  style={{
+                    background: '',
+                    border: 'none',
+                    fontSize: '1rem',
+                    cursor: 'pointer',
+                    transition: 'background-color 0.3s ease',
+                    borderRadius: '50%'
+                    
+                  }}
+                  // onMouseEnter={(e) => {
+                  //   e.target.style.backgroundColor = 'rgba(0, 0, 0, 0.1)'; // Hover background color
+                  // }}
+                  // onMouseLeave={(e) => {
+                  //   e.target.style.backgroundColor = ''; // Reset background on mouse leave
+                  // }}
+                  onClick={()=>handleTagClick(tag)}>&times;</button>
+                {/* <i
+                  className="fas fa-times custom-close"
+                  onClick={() => handleTagClick(tag)}
+                  aria-label="Close"
+                ></i> */}
+                {/* font-size: 1.5rem; */}
+              </div>
+            );
+          })}
         </div>
-      }
+      )}
 
       {/* card grid */}
       <div className="row">
