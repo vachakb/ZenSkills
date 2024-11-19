72fbc4a35835113ce96dfcf899c0ce011bda5fa2
Vacha <vachakb@gmail.com>
2024-11-19 12:54:20 +0100

    add exists function to all models

--- backend/models/prismaClient.js
+++ backend/models/prismaClient.js
@@ -1,5 +1,15 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient, Prisma } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+  model: {
+    $allModels: {
+      async exists(where) {
+        const context = Prisma.getExtensionContext(this);
+        const result = await context.findFirst({ where });
+        return result !== null;
+      },
+    },
+  },
+});
 
 module.exports = prisma;
