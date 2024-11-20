f45feb0b42baa0d0dfd30716d71f32a2f4740da7
Vacha <vachakb@gmail.com>
2024-11-21 00:55:52 +0100

    add validation for login and register

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,7 +6,9 @@ const {
 } = require("../controllers/registerUserController");
 const passport = require("passport");
 const { verify } = require("crypto");
+const validation = require("../middlewares/validation");
 const MagicLinkStrategy = require("passport-magic-link").Strategy;
+const yup = require("yup");
 
 passport.serializeUser((user, done) => {
   process.nextTick(() => {
@@ -26,6 +28,19 @@ passport.use("local", authController.login);
 // TODO error handling
 router.post(
   "/login",
+  validation(
+    yup.object({
+      email: yup
+        .string()
+        .email("Must be a valid email")
+        .required("This is a required field"),
+      password: yup
+        .string()
+        .required("This is a required field")
+        .min(8, "Password should be at least 8 characters"),
+      role: yup.string().oneOf(["mentor", "mentee"], null).required(),
+    }),
+  ),
   passport.authenticate("local", { failWithError: true }),
   (_, res) => {
     res.sendStatus(200);
@@ -81,7 +96,30 @@ router.get(
 router.post("/google/callback", authController.googleCallback);
 
 // Registration route
-router.post("/register", authController.register);
+router.post(
+  "/register",
+  validation(
+    yup.object({
+      email: yup
+        .string()
+        .email("Must be a valid email")
+        .required("This is a required field"),
+      password: yup
+        .string()
+        .required("This is a required field")
+        .min(8, "Password should be at least 8 characters"),
+      phoneNum: yup
+        .string()
+        .required("This is a required field")
+        .test(
+          "is-valid-phone",
+          "Phone number is invalid",
+          (value) => value && value.length >= 10, // Or use a regex for more accuracy
+        ),
+    }),
+  ),
+  authController.register,
+);
 
 // To register other fields of user like name, email, password
 router.post("/register-user", registerUserProfile);
