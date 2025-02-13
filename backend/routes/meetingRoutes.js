449d346dafbbb0b530d840de090679a979343d72
Vacha <vachakb@gmail.com>
2025-02-13 22:43:18 +0100

    add routes for recording endpoints

--- backend/routes/meetingRoutes.js
+++ backend/routes/meetingRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const { getToken, createRoom } = require("../controllers/meetingController");
+const {
+  getToken,
+  createRoom,
+  startRecording,
+  stopRecording,
+} = require("../controllers/meetingController");
 
 const router = express.Router();
 
 router.post("/token", getToken);
 
 router.post("/room", createRoom);
+
+router.post("/recordings/start", startRecording);
+
+router.post("/recordings/stop", stopRecording);
 
 module.exports = router;
