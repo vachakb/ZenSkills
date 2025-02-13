103edbde3806c2f30289f56182521ba7a08cb078
Vacha <vachakb@gmail.com>
2025-02-13 22:43:44 +0100

    remove start/stop recording button, record entire meeting from start to finish

--- frontend/src/apis/meeting.js
+++ frontend/src/apis/meeting.js
@@ -8,4 +8,12 @@ function createRoom(token) {
   return axiosInstance.post("/meetings/room", { token });
 }
 
-export { getToken, createRoom };
+function startRecording(token, roomId) {
+  return axiosInstance.post("/meetings/recordings/start", { token, roomId });
+}
+
+function stopRecording(token, roomId) {
+  return axiosInstance.post("/meetings/recordings/stop", { token, roomId });
+}
+
+export { getToken, createRoom, startRecording, stopRecording };
